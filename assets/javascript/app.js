b24072ff153ec1ad30a083547561844750c5165f
ginnac <ginnacampbell1990@gmail.com>
2019-06-09 20:44:14 -0600

    functional trivia game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,41 +18,40 @@ question3 ={
     rightAnswer: "Toalla",
     src: "assets/images/toalla.jpg"  
 }];
+
 var interval;
 var intervalSec;
 var intervalTimeOut;
 var answerIndex;
 var array;
+var index = 0;
 var count = 31;
 var textAnswer;
 var correctAnswersScore= 0;
 var unansweredScore =0;
 var incorrectAnswersScore =0;
-//lets start from a random value in our objectNamesarray
-//var finalObjectName = objectsNames[Math.floor(Math.random() * objectsNames.length)];
-//console.log(finalObjectName);
-var index = 0;
+
 //start function when document is ready
 $( document ).ready(function() {
-
- //   ID content needs to be hidden  until start function is called displayed
+ //   ID content needs to be hidden  until start function is called displayed and stats ID should be shown when finishing the Trivia Game:
     function hideContent(){ 
     $("#content").hide();
     }
+    
     function hideStats(){
         $("#stats").hide();
     }
+
     hideContent();
     hideStats();
 
-//start function will be called to ID content diplays and questions start
+//start function will be called to make ID content diplays and questions start
    function startbuttom(){
     $("#start-button").on("click",function(){
     $("#content").show();
     $("#start-button").hide();
     displayquestions();
     loopOfQuestions();
-    intervalTimer();
     });
     }
 
@@ -63,6 +62,12 @@ $( document ).ready(function() {
     //display questions:
     $("#question").text(objectsNames[index].name);
     displayAnswers();
+    intervalTimer();
+    $("#answer-tabs").show();
+    $("#pic").hide();
+    $("#result").hide();
+    $("#correctAnsweris").hide();
+   
    }
 
 //function to display answers: 
@@ -87,137 +92,143 @@ function displayAnswers(){
         
         $("#answer4").text(objectsNames[index].answers);
 
+        //restoring our arrays - 
         objectsNames[0].answers = ["Tristeza","Cordura","Tranquilidad","Felicidad"];
         objectsNames[1].answers = ["Pies", "Manos", "Codos", "Menton"];
         objectsNames[2].answers = [ "Toalla", "Frazada", "Tina", "Toro"];
     }
 
 //function to pass to next question
-function nextQuestion(){
-    // setTimeout(function(){quickReview()},500);
-    index++;
-    intervalTimeOut = setTimeout(function(){displayquestions();},1000);
-    $("#answer-tabs").show()
-
+    function nextQuestion(){
+        clearInterval(intervalSec);
+        //first let's get the answer- 
+        quickReview();
+        //pass to next index (objectNames array)
+        index++;
+        //wait 2 secs to display next index...
+        intervalTimeOut = setTimeout(function(){displayquestions();},2000);
  };
 
- //function for interval so functions can happen again and again until reaches the total length of the Objectnames array:
-function loopOfQuestions(){
-    interval = setInterval(function(){nextQuestion();},30000);
-}
-
-//function to get the time left show in the DOM: 
-function timer()
-{ $("#timer").text(count + " " + "seconds left");
-count=count-1;
-$("#timer").text(count + " " + "seconds left");
- if(count==0){
-count=31;
-unansweredScore = unansweredScore+1;}
-}
-
-//function for intervals for the seconds countdown
-function intervalTimer(){
-    count = 31;
-    intervalSec = setInterval(function(){timer();},1000);
-}
-
-
-// function to clear interval when all questions have been answered or 
-function stopAndNextQuestion(){
-    clearInterval(interval);
-    clearInterval(intervalSec);
-    clearTimeout(intervalTimeOut);
-}
-
-// function to update score
-    function score(){
-//correct answers
-$(".answer").on("click",function(){
-    textAnswer = $(this).text();
-    var Object3AnswersArray = objectsNames[2].answers
-    console.log(textAnswer);
-
-    if(Object3AnswersArray.includes(textAnswer)) {
-        console.log(Object3AnswersArray);
+ //function for inmmediate results after a question: 
+    function quickReview(){  
+        $("#pic").show();
+        $("#answer-tabs").hide();
+        $("#result").show();
+        $("#correctAnsweris").show();
+        clearInterval(intervalSec);
+        $("#pic").attr("src",objectsNames[index].src);
+            $("#pic").attr("height","200px");
+            $("#pic").attr( "width","200px");
         if(textAnswer === objectsNames[index].rightAnswer){
-        correctAnswersScore = correctAnswersScore+1;
-        stopAndNextQuestion();
-        gameEnd();
+            $("#result").text("Correct");
+            $("#correctAnsweris").text("Correct answer is:" + " " + objectsNames[index].rightAnswer);            
         }
+
         else{
-        incorrectAnswersScore = incorrectAnswersScore+1;
-        stopAndNextQuestion();
-        gameEnd();  
+            $("#result").text("Incorrect");
+            $("#correctAnsweris").text("Correct answer is:" + " " + objectsNames[index].rightAnswer);        
         }
     }
 
+    //function for interval so questions can be displayed again and again until reaches the total length of the Objectnames array:
+    function loopOfQuestions(){
+        interval = setInterval(function(){nextQuestion();},31000);
+    }
 
-    else if(textAnswer === objectsNames[index].rightAnswer){
+    //function to get the time left show in the DOM: 
+    function timer(){
+        $("#timer").text(count + " " + "seconds left");
+        count=count-1;
+        $("#timer").text(count + " " + "seconds left");
+        if(count==0){count=31;
+            clearInterval(intervalSec);
+            clearInterval(interval);
+            nextQuestion();
+            unansweredScore = unansweredScore+1;
+            var quest = $("#question").text()
+            if(quest===objectsNames[objectsNames.length-1].name){
+                setTimeout(function(){stopAndNextQuestion();gameEnd();},2000); 
+            }
+        }
+    }
+
+    //function for intervals for the seconds countdown
+    function intervalTimer(){ 
+        count = 31;
+        intervalSec = setInterval(function(){timer();},1000);
+    }
+
+    // function to clear interval- will be called when all questions have been answered or 
+    function stopAndNextQuestion(){
+        clearInterval(interval);
         clearInterval(intervalSec);
-        intervalTimer();
-        nextQuestion();
-        correctAnswersScore = correctAnswersScore+1;
+        clearTimeout(intervalTimeOut);
+    }
+
+    // function to update score
+    function score(){
+        $(".answer").on("click",function(){
+            textAnswer = $(this).text();
+            var Object3AnswersArray = objectsNames[objectsNames.length-1].answers
+        //if we are dealing with the last object of the objectNames array
+        if(Object3AnswersArray.includes(textAnswer)) {
+            //if it is a correct answer
+            if(textAnswer === objectsNames[index].rightAnswer){
+            correctAnswersScore = correctAnswersScore+1;
+            quickReview();
+            setTimeout(function(){stopAndNextQuestion();gameEnd();},2000);
+            }
+            //if it is an incorrect answer 
+            else{
+            incorrectAnswersScore = incorrectAnswersScore+1;
+            quickReview();
+            setTimeout(function(){stopAndNextQuestion();gameEnd();},2000);
+            }
+        }
+        // if we are not dealing with the last object , and it is correct answer
+        else if(textAnswer === objectsNames[index].rightAnswer){
+            clearInterval(intervalSec);
+            clearInterval(interval);
+            nextQuestion();
+            correctAnswersScore = correctAnswersScore+1;
         }
 
-//incorrect answers
-    else{
-        clearInterval(intervalSec);
-        intervalTimer();
-        nextQuestion();
-        incorrectAnswersScore = incorrectAnswersScore+1;
-
-    }
-});
-}
+        //if we are not dealing with the last object , and it is an incorrect answer
+        else{
+            clearInterval(intervalSec);
+            clearInterval(interval);
+            nextQuestion();
+            incorrectAnswersScore = incorrectAnswersScore+1;
+        }
+        });
+    }    
 score();
 
-//View of the score
- function gameEnd(){
-    hideContent();
-    $("#stats").show();
-    $("#correct-stats").text("Correct Answers:"+ " " +correctAnswersScore);
-    $("#incorrect-stats").text("incorrect Answers:"+ " " + incorrectAnswersScore);
-    $("#unanswered-stats").text("Unanswered:" + " " + unansweredScore);
-};
+    //View of the score
+    function gameEnd(){
+        hideContent();
+        $("#stats").show();
+        $("#correct-stats").text("Correct Answers:"+ " " +correctAnswersScore);
+        $("#incorrect-stats").text("incorrect Answers:"+ " " + incorrectAnswersScore);
+        $("#unanswered-stats").text("Unanswered:" + " " + unansweredScore);
+    };
 
- //reset game 
-
- function reset(){
-$("#start-again-button").on("click",function(){
-        index = 0;
-        count = 31;
-        correctAnswersScore= 0;
-        unansweredScore =0;
-        incorrectAnswersScore =0;
-
-        hideStats();
-        $("#content").show();
-        $("#start-button").hide();
-        intervalTimer();
-        displayquestions();
-        loopOfQuestions();
-    });
- }
- reset();
-
-function quickReview(){
-    $("#answer-tabs").hide();
-    if(textAnswer === objectsNames[index].rightAnswer){
-        $("#result").text("Correct");
-        $("#correctAnsweris").text("Correct answer is:" + " " + objectsNames[index].rightAnswer);        
-        $("#pic").attr("src",objectsNames[index].src);
-        $("#pic").attr("height","200px");
-        $("#pic").attr( "width","200px");
-    }
-  
-
- }
- quickReview();
  
-
-
-
-   
+    //reset game 
+    function reset(){
+        $("#start-again-button").on("click",function(){
+            index = 0;
+            count = 31;
+            correctAnswersScore= 0;
+            unansweredScore =0;
+            incorrectAnswersScore =0;
+            hideStats();
+            $("#content").show();
+            $("#start-button").hide();
+            displayquestions();
+            loopOfQuestions();
+        });
+    }
+ reset();  
 });
 
